d1e93f75429b5aef446845cb103657ac448b7589
Sebastian Gibb <mail@sebastiangibb.de>
2017-07-28 23:07:28 +0200

    add new dataset (myo, ca, h3_3, h4, c3a)

---
+++ R/topDownDataPath.R
@@ -0,0 +1,23 @@
+#' TopDown Proteomic datasets
+#'
+#' This function returns the path to the example files accompanying
+#' the [topdown::topdown-package].
+#'
+#' @param protein `character`, name of the dataset.
+#'
+#' @return `character`, path to the directory containing the example
+#' files.
+#' @author Sebastian Gibb \email{mail@@sebastiangibb.de}
+#' @seealso [topdown::topdown-package]
+#' @export
+#' @examples
+#' library("topdowndata")
+#' topDownDataPath("myoglobin")
+topDownDataPath <- function(protein=c("myoglobin", "ca", "h3_3", "h4", "c3a")) {
+  path <- c(myoglobin="2017_06_29_myo",
+            ca="2017_07_03_CA",
+            "h3_3"="2017_07_05_H3_3",
+            h4="2017_07_07_H4",
+            c3a="2017_07_08_C3A")
+  system.file(path[match.arg(protein)], package="topdowndata")
+}
--- README.md
+++ README.md
@@ -4,4 +4,4 @@
 [![build status](https://travis-ci.org/sgibb/topdowndata.svg?branch=master)](https://travis-ci.org/sgibb/topdown?branch=master)
 [![license](http://img.shields.io/badge/license-GPL%20%28%3E=%203%29-brightgreen.svg?style=flat)](http://www.gnu.org/licenses/gpl-3.0.html)
 
-This package contains Histone H2A data generated on a Thermo Orbitrap Lumos MS device.
+Example data for the topdown package generated on a Thermo Orbitrap Lumos MS device.
